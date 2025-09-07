0587f042e72afae08d7c76c1e1ce1765a70b9b76
jithendraCodes <jithendradusanapudi99@gmail.com>
2025-09-07 14:00:16 +0530

    practice

---
+++ Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 1;
+        System.out.println(a);
+    }
+}
--- sample2.py
+++ sample2.py
@@ -1,2 +1,4 @@
 #this is sample python
 print("hello world")
+a=1
+print(a)
